3d35cf0d35f7ac09c1de48f326de9eaafaeb440c
jacky <jagardiner02@gmail.com>
2025-04-19 14:26:18 +1000

    got charger back

--- arch_test/i2c/bus_scan.c
+++ arch_test/i2c/bus_scan.c
@@ -23,7 +23,7 @@
 #include "pico/stdlib.h"
 #include "pico/binary_info.h"
 #include "hardware/i2c.h"
-
+#include "hardware/uart.h"
 // Pico W devices use a GPIO on the WIFI chip for the LED,
 // so when building for Pico W, CYW43_WL_GPIO_LED_PIN will be defined
 #ifdef CYW43_WL_GPIO_LED_PIN
@@ -31,7 +31,7 @@
 #endif
 
 #ifndef LED_DELAY_MS
-#define LED_DELAY_MS 25
+#define LED_DELAY_MS 500
 #endif
 
 // Perform initialisation
@@ -60,58 +60,78 @@ void pico_set_led(bool led_on) {
 // I2C reserves some addresses for special purposes. We exclude these from the scan.
 // These are any addresses of the form 000 0xxx or 111 1xxx
 bool reserved_addr(uint8_t addr) {
-    return (addr & 0x78) == 0 || (addr & 0x78) == 0x78;
+    return false;
+    //return (addr & 0x78) == 0 || (addr & 0x78) == 0x78;
 }
-
 int main() {
-    // Enable UART so we can print status output
-    stdio_init_all();
-    int rc = pico_led_init();
-    hard_assert(rc == PICO_OK);
-    while (true) {
-        pico_set_led(true);
-        sleep_ms(LED_DELAY_MS);
-        pico_set_led(false);
-        sleep_ms(LED_DELAY_MS);
-    }
-#if !defined(i2c_default) || !defined(PICO_DEFAULT_I2C_SDA_PIN) || !defined(PICO_DEFAULT_I2C_SCL_PIN)
-#warning i2c/bus_scan example requires a board with I2C pins
-    puts("Default I2C pins were not defined");
-#else
-    // This example will use I2C0 on the default SDA and SCL pins (GP4, GP5 on a Pico)
+    // Set the GPIO pin mux to the UART - pin 0 is TX, 1 is RX; note use of UART_FUNCSEL_NUM for the general
+    // case where the func sel used for UART depends on the pin number
+    // Do this before calling uart_init to avoid losing data
+    gpio_set_function(0, UART_FUNCSEL_NUM(uart0, 0));
+    gpio_set_function(1, UART_FUNCSEL_NUM(uart0, 1));
+
+    // Initialise UART 0
+    uart_init(uart0, 115200);
+
+    uart_puts(uart0, "\nHello world!\n");
     i2c_init(i2c_default, 100 * 1000);
+
     gpio_set_function(PICO_DEFAULT_I2C_SDA_PIN, GPIO_FUNC_I2C);
     gpio_set_function(PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C);
     gpio_pull_up(PICO_DEFAULT_I2C_SDA_PIN);
     gpio_pull_up(PICO_DEFAULT_I2C_SCL_PIN);
     // Make the I2C pins available to picotool
     bi_decl(bi_2pins_with_func(PICO_DEFAULT_I2C_SDA_PIN, PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C));
-
-    printf("\nI2C Bus Scan\n");
-    printf("   0  1  2  3  4  5  6  7  8  9  A  B  C  D  E  F\n");
-
-    for (int addr = 0; addr < (1 << 7); ++addr) {
-        if (addr % 16 == 0) {
-            printf("%02x ", addr);
-        }
+    int target_address = 64;
+    int led_add = 0;
+    int deb = 1;
+    for (int addr = 0; addr < 256; ++addr) {
+        //if (addr % 16 == 0) {
+            //printf("%02x ", addr);
+        //}
 
         // Perform a 1-byte dummy read from the probe address. If a slave
         // acknowledges this address, the function returns the number of bytes
         // transferred. If the address byte is ignored, the function returns
         // -1.
-
         // Skip over any reserved addresses.
         int ret;
         uint8_t rxdata;
-        if (reserved_addr(addr))
+        /*
+        if (reserved_addr(addr)){
             ret = PICO_ERROR_GENERIC;
-        else
+            deb = 2;
+        }else{
             ret = i2c_read_blocking(i2c_default, addr, &rxdata, 1, false);
-
-        printf(ret < 0 ? "." : "@");
-        printf(addr % 16 == 15 ? "\n" : "  ");
+            if (ret >= -1){
+                led_add = 10;
+                deb = 4;
+            }
+        }
+        */
+        ret = i2c_read_blocking(i2c_default, addr, &rxdata, 1, false);
+        sleep_ms(100);
+        uart_puts(uart0,ret < 0 ? "NO DICE\n" : "DICE!!\n");
+        //uart_puts(uart0,addr % 16 == 15 ? "\n" : "  ");
     }
-    printf("Done.\n");
-    return 0;
-#endif
+    if (reserved_addr(5)){
+        deb = 3;
+    }
+    // Enable UART so we can print status output
+    //stdio_init_all();
+    int rc = pico_led_init();
+    hard_assert(rc == PICO_OK);
+    int i = 0;
+
+    pico_set_led(false);
+    sleep_ms(1000);
+    while (i < deb) {
+        pico_set_led(true);
+        sleep_ms(LED_DELAY_MS);
+        pico_set_led(false);
+        sleep_ms(LED_DELAY_MS);
+        i += 1;
+
+        uart_puts(uart0, "\n PING");
+    }
 }
